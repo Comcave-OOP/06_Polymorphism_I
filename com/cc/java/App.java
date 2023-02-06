97acef297ea8d7752eada45f7d8880c96ca91b98
Marinus <marinus.fehling@web.de>
2023-02-06 10:12:23 +0100

    überschriebene Methode doYourJob()

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,6 +3,18 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
+
+        Queen queen = new Queen();
+        Drone drone = new Drone();
+        Worker worker = new Worker();
+
+        output(queen.doYourJob());
+        output(drone.doYourJob());
+        output(worker.doYourJob());
+
+        output(queen.fly());
+        output(drone.fly());
+        output(worker.fly());
      
     }
 
